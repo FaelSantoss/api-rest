692709640e4f59707fd39ab01986b3149a770cfd
FaelSantoss <silva.faelm@gmail.com>
2024-06-30 00:35:10 -0300

    created cookie with fastify

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,25 @@
 import fastify from 'fastify'
 import { env } from './env'
 import { transactionsRoutes } from './routes/transactions'
+import fastifyCookie from '@fastify/cookie'
+import crypto from 'crypto'
 
 const app = fastify()
+
+app.register(fastifyCookie, {
+  secret: 'your-secret-key',
+})
+
+app.addHook('preHandler', (request, reply, done) => {
+  if (!request.cookies.sessionId) {
+    const sessionId = crypto.randomUUID()
+    reply.setCookie('sessionId', sessionId, {
+      path: '/',
+      httpOnly: true,
+    })
+  }
+  done()
+})
 
 app.register(transactionsRoutes, {
   prefix: 'transactions',
